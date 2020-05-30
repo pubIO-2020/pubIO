21a18b378c1ddaebd4edbb29c94d1c94eba48a9c
Andrew Venson <vensoneandrew@gmail.com>
2020-05-30 16:45:26 -0500

    added tabbed navigator

---
+++ pubio/components/User.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Header from "./Header";
+
+export default function User() {
+  return (
+    <View>
+      <Header />
+      <View>
+        <Text>User</Text>
+      </View>
+    </View>
+  );
+}
