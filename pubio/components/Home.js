83b3a2b11c87b25dac220d88a745cb27053b0d76
New Name <mubin.momin@nxp.com>
2020-05-30 17:29:02 -0500

    added the update to cardview and mapview

--- pubio/components/Home.js
+++ pubio/components/Home.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Cardview from "./screens/Cardview";
+import Mapview from "./screens/Mapview";
 
-import Header from "./Header";
+const Stack = createStackNavigator();
 
 export default function Home() {
   return (
-    <View>
-      <Header />
-      <View>
-        <Text>Home</Text>
-      </View>
-    </View>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator screenOptions = {{headerShown: false, animationEnabled: false}}>
+        <Stack.Screen name="Cardview" component={Cardview} />
+        <Stack.Screen name="Mapview" component={Mapview} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
